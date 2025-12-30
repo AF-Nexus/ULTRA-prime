08f022fb1b5f3c65adef0574993bd4cb375585ba
AF-Nexus <afnexus01@gmail.com>
2025-12-30 23:15:57 +0200

    Update Paste.js

--- Paste.js
+++ Paste.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Multiple API keys for fallback
 const API_KEYS = [
-    'CNJgmfg9X745hcnMQ7FE-nDxytt6w8xK',
+    '',
     'Pe8nyDTO5Jm4ZdKo3qlbKjaFSP53srbT',
     'c7Jo_q9xvCMAQsj1qihjLJBMBY2Er5--',
     'KpoS0JysNXgUSgCWH2hr__2OG7aJ30S_',
