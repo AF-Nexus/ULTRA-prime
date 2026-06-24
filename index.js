7ed4779a423b3367800ad585d1449d39328a99c9
AF-Nexus <afnexus01@gmail.com>
2026-06-24 09:43:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
-    code = require('./pair');
+    code = require('./pair'),
+    code2 = require('./pair2');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
 app.use('/code', code);
+app.use('/code2', code2);
+app.use('/pair2', async (req, res, next) => {
+res.sendFile(__path + '/pair2.html')
+})
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
